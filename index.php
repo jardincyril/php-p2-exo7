8385636358f95db205242877fe7facddd7d6885d
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:36:58 +0200

    condition

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $isOk = true;
+    if ($isOk == false) {
+      echo "c\'est pas bon !!!";
+    }
+    else {
+      echo "c\'est ok !!";
+    }
+     ?>
+  </body>
+</html>
